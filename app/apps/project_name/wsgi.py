6906760587e0a8bab5f9802cbb075a23ee7b993b
theduke <Christoph Herzog>
2014-02-15 17:47:58 +0100

    in wsgi.py, lib dir and pyenv dirs are now appended to the pythonpath

--- app/apps/project_name/wsgi.py
+++ app/apps/project_name/wsgi.py
@@ -8,6 +8,16 @@ https://docs.djangoproject.com/en/{{ docs_version }}/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Add apps and lib directories to the python path.
+DIR = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.dirname(DIR))
+
+from {{ project_name }} import settings
+sys.path.append(settings.VIRTENV_DIR)
+sys.path.append(os.path.join(settings.BASE_DIR, 'lib'))
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
 
 from django.core.wsgi import get_wsgi_application

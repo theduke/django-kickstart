9d7ece1f1f6b219167e91d0745c58a7515d9d47b
theduke <chris@theduke.at>
2014-03-28 21:58:44 +0100

    Preparing 0.1.6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name = 'django-kickstart',
-    version = '0.1.5',
+    version = '0.1.6',
     packages = ['django_kickstart'],
     include_package_data = True,
     entry_points = {

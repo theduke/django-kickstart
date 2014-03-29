99177c2dd741385ad2c108cac2a4c681a58b3233
theduke <chris@theduke.at>
2014-03-29 08:26:16 +0100

    Added admin.autodiscover() which was missing.

--- django_kickstart/template/app/apps/project_name/urls.py
+++ django_kickstart/template/app/apps/project_name/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 
+admin.autodiscover()
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', '{{ project_name }}.views.home', name='home'),
